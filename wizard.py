691bb94f1fab5cc582e2d3942a68eabaf36bb07a
rajbot <raj@archive.org>
2014-02-04 21:44:50 -0800

    Add test implementation of lookupCD query.

--- wizard.py
+++ wizard.py
@@ -2,6 +2,9 @@
 
 from PyQt4 import QtCore, QtGui
 import sys
+import json
+import urllib
+
 
 # ArchiveWizard
 #_________________________________________________________________________________________
@@ -12,10 +15,12 @@ class ArchiveWizard(QtGui.QWizard):
         self.intro_page        = IntroPage(self)
         self.scan_drives_page  = ScanDrivesPage(self)
         self.read_toc_page     = ReadTOCPage(self)
+        self.lookup_cd_page    = LookupCDPage(self)
 
         self.addPage(self.intro_page)
         self.addPage(self.scan_drives_page)
         self.addPage(self.read_toc_page)
+        self.addPage(self.lookup_cd_page)
 
 
 # WizardPage
@@ -92,7 +97,7 @@ class ScanDrivesPage(WizardPage):
 class ReadTOCPage(WizardPage):
     def __init__(self, wizard):
         WizardPage.__init__(self, wizard)
-        self.setTitle('Looking up CD in IA database')
+        self.setTitle('Reading CD Table of Contents')
 
         self.toc_label = QtGui.QLabel('checking...')
         self.toc_string = None
@@ -122,8 +127,41 @@ class ReadTOCPage(WizardPage):
         self.toc_string = disc.toc_string
 
         print self.toc_string
-        self.toc_label.setText(self.toc_string)
+        self.toc_label.setText(self.toc_string + '\n\nPress Next to check the archive.org database')
         self.is_complete = True
+
+
+# LookupCDPage
+#_________________________________________________________________________________________
+class LookupCDPage(WizardPage):
+    def __init__(self, wizard):
+        WizardPage.__init__(self, wizard)
+        self.setTitle('Looking up CD in archive.org database')
+
+        self.status_label = QtGui.QLabel('checking...')
+
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.status_label)
+
+        self.setLayout(layout)
+        self.is_complete = False
+
+
+    def initializePage(self):
+        url = 'http://dowewantit0.us.archive.org:5000/lookupCD?'
+        url += urllib.urlencode({'sectors': self.wizard.read_toc_page.toc_string})
+        #test_toc = '1 10 211995 182 22295 46610 71440 94720 108852 132800 155972 183515 200210'
+        #url += urllib.urlencode({'sectors': test_toc})
+        print url
+
+        f = urllib.urlopen(url)
+        c = f.read()
+        self.status_label.setText('server returned:\n\n' + c)
+        print c
+
+
+    def isComplete(self):
+        return self.is_complete
 
 
 if __name__ == "__main__":
